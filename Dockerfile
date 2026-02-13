a27205d4487e4acb2389a0baa2c6e03015c669b4
rgarcia2304 <garcia2304@icloud.com>
2026-02-13 10:06:39 -0500

    Creating publisher and Queue

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM rabbitmq:3.13
+RUN rabbitmq-plugins enable rabbitmq_stomp
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+	"github.com/rgarcia2304/internal/pubsub"
+	"github.com/rgarcia2304/internal/routing"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil{
+		fmt.Println(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection Was Successful")
+
+	channel, err := connection.Channel()
+	
+	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	
 }
